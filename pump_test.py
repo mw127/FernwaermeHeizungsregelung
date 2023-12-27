4b07b7343c1072068e05d6a7f9e0de68ffc063ba
mw127 <66553540+mw127@users.noreply.github.com>
2023-12-27 20:28:14 +0100

    Create pump_test.py
    Eine kleine Routine um die Ansteuerung der Umwälzpumpe zu testen.

---
+++ pump_test.py
@@ -0,0 +1,20 @@
+# Eine kleine Routine um die Ansteuerung der Umwälzpumpe zu testen
+from gpiozero import Button, DigitalOutputDevice
+
+PIN_PUMP = 27
+pump = DigitalOutputDevice(PIN_PUMP)
+
+try:
+    while True:
+        strinput = input("Pumpe on/off: ")
+        if strinput == "on":
+            pump.on()
+        elif strinput == "off":
+            pump.off()
+        else:
+            break
+
+except KeyboardInterrupt:
+    print("ende")
+
+pump.off()
